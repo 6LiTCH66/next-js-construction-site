f873928c11c962e73078f408b1f243aba162358a
iljah <ilja200303@gmail.com>
2023-02-27 21:05:31 +0200

    translation for contact page was added

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -5,6 +5,7 @@ import stylesForm from "@/styles/ContactForm.module.css"
 import React, {useState} from "react";
 
 export default function Contact() {
+    const { t } = useTranslation();
     const [name, setName] = useState(" ")
     const [email, setEmail] = useState(" ")
 
@@ -30,46 +31,52 @@ export default function Contact() {
         <div className={styles.contactUsBox}>
             <div className={styles.contactUsHeader}>
                 <div className={styles.contactUsHeaderWrapper}>
-                    <p className={styles.contactUsTitle}>Kонтакты</p>
+                    <p className={styles.contactUsTitle}>{t("contact.title")}</p>
                     <hr/>
-                    <p className={styles.contactUsDescription}>Свяжитесь с нами и получите бесплатную профессиональную консультацию по всем строительным вопросам.</p>
+                    <p className={styles.contactUsDescription}>
+                        {t("contact.description")}
+                    </p>
                 </div>
             </div>
             <div className={styles.contactUsContainer}>
                 <div className={styles.contactUsWrapper}>
                     <div className={styles.contactUsForm}>
-                        <p className={`${styles.contactUsInfoTitle} ${styles.mobile}`}>Вы можете связаться с нами, позвонив или отправив сообщение.</p>
+                        <p className={`${styles.contactUsInfoTitle} ${styles.mobile}`}>
+                            {t("contact.info_title")}
+                        </p>
                         <div className={stylesForm.inputContainer}>
                             <div className={`${stylesForm.formInput} ${name ? "" : stylesForm.errorHelperText}`}>
-                                <input type="text" placeholder="Имя" id="first_name" onChange={nameChange}/>
-                                <label htmlFor="first_name" className={stylesForm.required}>Имя</label>
+                                <input type="text" placeholder={t("contact_form.name_input")} id="first_name" onChange={nameChange}/>
+                                <label htmlFor="first_name" className={stylesForm.required}>{t("contact_form.name_input")}</label>
                             </div>
                             {!name ? <p className={stylesForm.errorInput}>Обязательное поле</p> : <></>}
 
                         </div>
                         <div className={stylesForm.inputContainer} style={{marginTop: "40px"}}>
                             <div className={`${stylesForm.formInput} ${email ? "" : stylesForm.errorHelperText}`}>
-                                <input type="email" placeholder="Э-почта" id="email" onChange={emailChange}/>
-                                <label htmlFor="email" className={stylesForm.required}>Э-почта</label>
+                                <input type="email" placeholder={t("contact_form.email_input")} id="email" onChange={emailChange}/>
+                                <label htmlFor="email" className={stylesForm.required}>{t("contact_form.email_input")}</label>
                             </div>
                             {!email ? <p className={stylesForm.errorInput}>Обязательное поле</p> : <></>}
                         </div>
 
                         <div className={`${stylesForm.formInput} ${stylesForm.contactFormRow}`}>
-                            <textarea id={stylesForm.workDescription} rows="5" placeholder="Описание работы"></textarea>
-                            <label htmlFor="workDescription" className="textarea-label">Описание работы</label>
+                            <textarea id={stylesForm.workDescription} rows="5" placeholder={t("contact_form.work_description_input")}></textarea>
+                            <label htmlFor="workDescription" className={stylesForm.textareaLabel}>{t("contact_form.work_description_input")}</label>
                         </div>
 
                         <div className={stylesForm.formButton}>
                             <button className={stylesForm.sendButton}>
-                                Отправить форму
+                                {t("contact_form.projects_button")}
                             </button>
                         </div>
 
                     </div>
 
                     <div className={styles.contactUsInfo}>
-                        <p className={styles.contactUsInfoTitle}>Вы можете связаться с нами, позвонив или отправив сообщение.</p>
+                        <p className={styles.contactUsInfoTitle}>
+                            {t("contact.info_title")}
+                        </p>
                         <div className={styles.contactUsTextContainer}>
                             <h2 className={`${styles.contactUsCompany} ${styles.contactUsText}`}>
                                 SEMARIM OÜ
@@ -77,9 +84,9 @@ export default function Contact() {
                                 Registrikood: 14439231
                             </h2>
                             <h2 className={styles.contactUsText}>
-                                Э-почта: <a href="mailto:rimgor@yandex.ru">rimgor@yandex.ru</a>
+                                {t("contact.email")} <a href="mailto:rimgor@yandex.ru">rimgor@yandex.ru</a>
                                 <br/>
-                                Tелефон: <a href="tel:+37258491096">+372 58491096</a>
+                                {t("contact.telephone")} <a href="tel:+37258491096">+372 58491096</a>
                             </h2>
                             <h2 className={styles.contactUsText}>
                                 Harju maakond, Maardu linn, Veeru tn 16-37, 74113
