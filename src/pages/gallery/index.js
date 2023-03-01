7cbe2b579724e19b16a9c5cf63f237cf76d8ff73
iljah <ilja200303@gmail.com>
2023-03-01 20:46:22 +0200

    modal window for image was added

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -3,31 +3,44 @@ import Image from "next/image"
 import fetchAllImages from "../../../utils/getImages"
 import styles from "@/styles/Projects.module.css";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import someImage from "../../../public/assets/consultation.jpg"
 export default function Gallery(){
     const [images, setImages] = useState([])
-
+    const [selectedImage, setSelectedImage] = useState(someImage)
     useEffect(() => {
         const images = fetchAllImages()
 
         setImages(images)
     }, []);
-
+    const getSrc = (event) =>{
+        event.preventDefault();
+        setSelectedImage(event.target.src)
+        console.log(event.target.src)
+    }
 
     return (
-        <div className={styles.projectsContainer}>
-            <div className={styles.projectsContainerText}>
+        <>
+            <div className={styles.projectsContainer}>
+                <div className={styles.projectsContainerText}>
                 <span className={styles.projectsTitle}>
                     Наши сделанные проекты
                 </span>
+                </div>
+                <div className={styles.projectsImageContainer}>
+                    {images.map((image, key) => (
+                        <div className={styles.imageWrapper} key={key}>
+                            <Image src={image} alt="image" className={styles.projectsImage} onClick={getSrc}/>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className={styles.projectsImageContainer}>
-                {images.map((image, key) => (
-                    <div className={styles.imageWrapper} key={key}>
-                        <Image src={image} alt="image" className={styles.projectsImage}/>
-                    </div>
-                ))}
+
+            <div id={styles.myModal} className={styles.modal}>
+                <span className={styles.close}>×</span>
+                <Image src={images[16]} alt="image" className={styles.modalContent}/>
             </div>
-        </div>
+        </>
+
     )
 }
 
