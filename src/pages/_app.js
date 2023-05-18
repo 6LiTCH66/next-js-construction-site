f9fc78b838680d78415f43399790d05b3781a38c
iljah <ilja200303@gmail.com>
2023-05-18 08:19:56 +0300

    google search console tag was added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import Layout from "../../components/layout";
 import useTranslation from "next-translate/useTranslation";
 import {Analytics} from "@vercel/analytics/react";
+import Head from "next/head"
 
 function App({ Component, pageProps }) {
     const { t } = useTranslation("common")
@@ -11,6 +12,9 @@ function App({ Component, pageProps }) {
           navbar_text={t("navbar", { }, {returnObjects: true})}
           footer_text={t("footer", { }, {returnObjects: true})}
       >
+          <Head>
+              <meta name="google-site-verification" content="2eBmX1VpUOVF3kh4vNmhl6V7q8J9ABP4-iuLJX2WEy8" />
+          </Head>
         <Component {...pageProps} />
           <Analytics/>
       </Layout>
