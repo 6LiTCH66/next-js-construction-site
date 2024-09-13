7cdd6f4f53fac55e45d609763ed47ebc5b7c5aee
ilja0303 <ilja200303@gmail.com>
2024-09-13 10:44:55 +0300

    video was uploaded to vercel blob

--- components/video_player/VideoPlayer.js
+++ components/video_player/VideoPlayer.js
@@ -6,7 +6,7 @@ const VideoPlayer = ({filename}) => {
     return (
         <div style={{flex: 1, display: "flex", alignItems: "center"}}>
             <video height="full" controls style={{maxWidth:'100%'}}>
-                <source src={`/assets/about_video/${filename}`} type="video/mp4" />
+                <source src="https://c3yxt1bigeceprsg.public.blob.vercel-storage.com/Semarim1-YKvg1NzOeTVkMGvbPa1Nyi2xRfAdsL.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
